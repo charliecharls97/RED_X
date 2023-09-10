99da5ababa41f86e3cdc7b3168fa99562dd2230a
charlie <carlosibgroup@gmail.com>
2023-09-09 22:22:39 -0500

    commit-style/login

--- projects/room_of_legends/src/app/form-login/form-login.component.ts
+++ projects/room_of_legends/src/app/form-login/form-login.component.ts
@@ -5,11 +5,32 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './form-login.component.html',
   styleUrls: ['./form-login.component.css']
 })
-export class FormLoginComponent implements OnInit {
 
-  constructor() { }
+export class FormLoginComponent implements OnInit {
+[x: string]: any;
+  public cardTitulo: string;
+  public selectedIndex: number;
+
+  constructor() {
+    this.cardTitulo = 'Ingresar';
+    this.selectedIndex = 0;
+  }
 
   ngOnInit(): void {
   }
+
+  cambiarTitulo(event: any) : void {
+    this.selectedIndex = event.index;
+
+    if (this.selectedIndex === 0) {
+      this.cardTitulo = 'Ingresar';
+    } else if (this.selectedIndex === 1) {
+      this.cardTitulo = 'Crear Cuenta';
+    }
+    
+  }
+
+
+  
 
 }
